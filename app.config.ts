6f1d94a4c41f9d9facc906783afeaa6b81d4c818
micahjonas <micha.schwendener@gmail.com>
2024-03-19 20:10:42 +0100

    fix vite config

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,12 @@
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    preset: "cloudflare-pages",
+    // We will need to enable CF Pages node compatiblity
+    // https://developers.cloudflare.com/workers/runtime-apis/nodejs/asynclocalstorage/
+    rollupConfig: {
+      external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
+    },
+  },
+});
