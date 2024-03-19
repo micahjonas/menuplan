56ae57e7e643cf2e494775f0b6858164bd7965c1
micahjonas <micha.schwendener@gmail.com>
2024-03-19 19:43:26 +0100

    Solid Start Basic Template + Cloudflare

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+export default defineConfig({
+  start: {
+    server: {
+      preset: "cloudflare-pages",
+      // We will need to enable CF Pages node compatiblity
+      // https://developers.cloudflare.com/workers/runtime-apis/nodejs/asynclocalstorage/
+      rollupConfig: {
+        external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
+      },
+    },
+  },
+})
